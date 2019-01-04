4494f8ab659a05be0beacc522ca3142cee394567
Alberto <info@androiddesdecero.com>
2019-01-04 12:40:27 +0100

    06

---
+++ app/src/main/java/com/androiddesdecero/cursodagger2/di/MotorModule.java
@@ -0,0 +1,18 @@
+package com.androiddesdecero.cursodagger2.di;
+
+import com.androiddesdecero.cursodagger2.Motor;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by albertopalomarrobledo on 4/1/19.
+ */
+
+@Module
+public class MotorModule {
+    @Provides
+    public Motor providesMotorDiesel(){
+        return new Motor("Diesel");
+    }
+}
