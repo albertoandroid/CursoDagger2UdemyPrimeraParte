ed05213ba11cf7f3025bac1ca1de21abb5b272d4
Alberto <info@androiddesdecero.com>
2019-01-04 19:19:45 +0100

    11

--- app/src/main/java/com/androiddesdecero/cursodagger2/di/MotorModule.java
+++ app/src/main/java/com/androiddesdecero/cursodagger2/di/MotorModule.java
@@ -4,6 +4,7 @@ import com.androiddesdecero.cursodagger2.Coche;
 import com.androiddesdecero.cursodagger2.Motor;
 
 import javax.inject.Named;
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -15,12 +16,14 @@ import dagger.Provides;
 @Module
 public class MotorModule {
 
+    @Singleton
     @Named("diesel")
     @Provides
     public Motor providesMotorDiesel(){
         return new Motor("Diesel");
     }
 
+    @Singleton
     @Named("gasolina")
     @Provides
     public Motor providesMotorGasolina(){
